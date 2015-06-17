f18ddeaede3791278111728d883430f0b1e701bd
Julio Mario Sosa O <jsosa@tecnosystems.co>
2015-06-17 16:15:30 -0500

    Relative paths fixed

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import os
 p = pyaudio.PyAudio()
 r = sr.Recognizer()
 while True:
-	with sr.Microphone() as source:                # use the default microphone as the audio source
-		audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
+	with sr.Microphone() as source:
+		audio = r.listen(source)
 	try:
 		recognized_text = r.recognize(audio).lower()
 		words = recognized_text.split(" ");
 		print words
-		print("You said: " + recognized_text)    # recognize speech using Google Speech Recognition	
+		print("You said: " + recognized_text)
 		if "shut" in words and "down" in words and"please" in words:
 			os.system('sudo shutdown -P now')  
 		elif "go" in words and "the" in words and "web" in words:
 			os.system('su teo -c google-chrome-stable')  
 		elif "hello" in words and "jarvis" in words:
-			os.system('play /home/teo/Documents/recognition/jarvissaluda.wav')  			
+			os.system('play jarvissaluda.wav')  			
 		elif "why" in words and "spanish" in words:
-			os.system('play /home/teo/Documents/recognition/whyinspanish.wav')  			
-	except LookupError:                            # speech is unintelligible
+			os.system('play whyinspanish.wav')  			
+	except LookupError:
 	    print("Could not understand audio")
